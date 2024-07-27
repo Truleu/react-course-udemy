3cc953b1bb714a4e8e66feb8cde7ee4c5b985dfe
jonas.cabral <jonas.cabral@processasistemas.com.br>
2024-07-27 15:14:20 -0300

    Initial commit

---
+++ section1/hello-world/README.md
@@ -0,0 +1,5 @@
+# The Ultimate React Course 2024: React, Next.js, Redux & More
+
+First section from the course `The Ultimate React Course 2024: React, Next.js, Redux & More` on Udemy
+
+
---
+++ section1/hello-world/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+export default function App() {
+  const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice ? advice : "Click button below"}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>You have read <strong>{props.count}</strong> pieces of advice</p>
+  );
+}
